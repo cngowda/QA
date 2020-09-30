77c0348495efa50015bebce3c899af5daa3ab13b
chandru n gowda <58328903+cngowda@users.noreply.github.com>
2020-09-30 20:39:37 +0530

    Update conftest.py

--- test_ufe/conftest.py
+++ test_ufe/conftest.py
@@ -1,16 +1,16 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--CSP_HOST", action="store", help="csp host")
-    parser.addoption("--CSP_USER", action="store", help="csp user")
+    parser.addoption("--CSP_H", action="store", help="csp host")
+    parser.addoption("--CSP_U", action="store", help="csp user")
     parser.addoption("--iteration", action="store", help="csp host")
     parser.addoption("--refresh", action="store", help="csp user")
 
 @pytest.fixture
 def params(request):
     params = {}
-    params['CSP_HOST'] = request.config.getoption('--CSP_HOST')
-    params['CSP_USER'] = request.config.getoption('--CSP_USER')
+    params['CSP_H'] = request.config.getoption('--CSP_H')
+    params['CSP_U'] = request.config.getoption('--CSP_U')
     params['iteration'] = request.config.getoption('--iteration')
     params['refresh'] = request.config.getoption('--refresh')
     if params['CSP_HOST'] is None or params['CSP_USER'] is None:
